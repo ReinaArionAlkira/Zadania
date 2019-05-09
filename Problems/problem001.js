28be4e693098e1d453a8c61ab065faf708c6495c
Renata <r.wesolowska@devesoft.pl>
2019-05-09 13:41:35 +0200

    dodanie problem001 wykonany

--- Problems/problem001.js
+++ Problems/problem001.js
@@ -1,1 +1,18 @@
-function triangleArea(a, b, c)
+const readlineSync = require('readline-sync');
+
+var a = readlineSync.questionInt();
+var b = readlineSync.questionInt();
+var c = readlineSync.questionInt();
+
+function triangleArea (a, b, c) {
+  if (a + b > c && a + c > b && c + b > a){
+    var p = (a + b + c)/2;
+    var homer = p*(p-a)*(p-b)*(p-c);
+    var pole =  Math.sqrt(homer);
+    pole = Math.round(pole * 100) / 100;
+    if (Number.isNaN(pole)) console.log("-1")
+    else console.log(pole)
+  }
+  else console.log("-1")
+}
+triangleArea (a, b, c);
