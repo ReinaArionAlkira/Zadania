078b8ba5c19830fdf2760c6d54c6dbaab2d82f04
Renata <r.wesolowska@devesoft.pl>
2019-05-10 09:46:39 +0200

    gotowe (tester generuje błąd)

--- Problems/problem002.js
+++ Problems/problem002.js
@@ -1,5 +1,17 @@
+[,,a,b] = process.argv;
 function stringRotate(a, b){
-
-  if(b>0) return();
-  return (a);
+  b = b*1;
+  if ((typeof a != "string") || (typeof b != "number"))
+    console.log("");
+  else if ((b < 0) || !(Number.isInteger(b)))
+    console.log("");
+  else if (b > a.length)
+    b = Math.floor(b/a.length);
+  var frag = a.toString().substr(0, b);
+  console.log("Frag:" + frag);
+  var end = a.toString().substr(b, a.length - b);
+  var result = "";
+  result = end + frag;
+  console.log(result);
 }
+stringRotate(a, b);
