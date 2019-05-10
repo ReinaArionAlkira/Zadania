e0d916f363b0b28fa2e8559abb280e6575f3b912
Renata <r.wesolowska@devesoft.pl>
2019-05-10 09:50:58 +0200

    wizualnie

--- Problems/problem002.js
+++ Problems/problem002.js
@@ -3,10 +3,13 @@ function stringRotate(a, b){
   b = b*1;
   if ((typeof a != "string") || (typeof b != "number"))
     console.log("");
+    
   else if ((b < 0) || !(Number.isInteger(b)))
     console.log("");
+
   else if (b > a.length)
     b = Math.floor(b/a.length);
+
   var frag = a.toString().substr(0, b);
   console.log("Frag:" + frag);
   var end = a.toString().substr(b, a.length - b);
