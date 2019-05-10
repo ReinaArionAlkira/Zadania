4da15a1aab892260ed80e50ef4baf7752e8b71ca
Renata <r.wesolowska@devesoft.pl>
2019-05-10 10:08:59 +0200

    wykonane

--- Problems/problem004.js
+++ Problems/problem004.js
@@ -1,3 +1,12 @@
 function getTotalNumberOfNewYaersEveSundays(a, b) {
-  
+  var number = 0;
+  for(var i = a; i <= b; i++){
+    var a = (i-1) % 100;
+    var b = (i-1) - a;
+    var c = a + Math.floor(a/4);
+    if(((((Math.floor(b / 100) % 4) * 5) + c) % 7) == 6)
+      number++;
+  }
+
+return number;
 }
