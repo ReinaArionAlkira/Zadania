ada352a15a6f827b7826b84a4556885abfea9736
Renata <r.wesolowska@devesoft.pl>
2019-05-10 09:51:50 +0200

    + sama funkcja

--- Problems/problem002.js
+++ Problems/problem002.js
@@ -3,7 +3,7 @@ function stringRotate(a, b){
   b = b*1;
   if ((typeof a != "string") || (typeof b != "number"))
     console.log("");
-    
+
   else if ((b < 0) || !(Number.isInteger(b)))
     console.log("");
 
@@ -18,3 +18,18 @@ function stringRotate(a, b){
   console.log(result);
 }
 stringRotate(a, b);
+
+/*
+function stringRotate(a, b){
+  b = b*1;
+  if ((typeof a != "string") || (typeof b != "number") || (b < 0) || !(Number.isInteger(b)))
+    return '';
+  else if (b > a.length)
+    b = Math.floor(b/a.length);
+  var frag = a.toString().substr(0, b);
+  var end = a.toString().substr(b, a.length - b);
+  var result = "";
+  result = end + frag;
+  return result;
+}
+*/
