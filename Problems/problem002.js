d132d1dff8bf1793c0c1ef7364e044bc5944e2b6
Renata <r.wesolowska@devesoft.pl>
2019-05-10 10:11:03 +0200

    umiejscowienie

--- Problems/problem002.js
+++ Problems/problem002.js
@@ -1,3 +1,17 @@
+function stringRotate(a, b){
+  b = b*1;
+  if ((typeof a != "string") || (typeof b != "number") || (b < 0) || !(Number.isInteger(b)))
+    return '';
+  else if (b > a.length)
+    b = Math.floor(b/a.length);
+  var frag = a.toString().substr(0, b);
+  var end = a.toString().substr(b, a.length - b);
+  var result = "";
+  result = end + frag;
+  return result;
+}
+//sprawdzenie w konsoli z podaniem
+/*
 [,,a,b] = process.argv;
 function stringRotate(a, b){
   b = b*1;
@@ -18,18 +32,4 @@ function stringRotate(a, b){
   console.log(result);
 }
 stringRotate(a, b);
-
-/*
-function stringRotate(a, b){
-  b = b*1;
-  if ((typeof a != "string") || (typeof b != "number") || (b < 0) || !(Number.isInteger(b)))
-    return '';
-  else if (b > a.length)
-    b = Math.floor(b/a.length);
-  var frag = a.toString().substr(0, b);
-  var end = a.toString().substr(b, a.length - b);
-  var result = "";
-  result = end + frag;
-  return result;
-}
 */
