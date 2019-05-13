fef6d1eab7093d09e3c00a16a8dbab36f938e22e
Renata <r.wesolowska@devesoft.pl>
2019-05-13 11:22:12 +0200

    niedokończone

--- Zad_cw_2/Zadanie.9.js
+++ Zad_cw_2/Zadanie.9.js
@@ -1,19 +1,16 @@
 const readlineSync = require('readline-sync');
 
 var liczba = readlineSync.questionInt();
-var dzielnik = 2;
-if (liczba <= 1)
-  liczba = readlineSync.questionInt();
+if (liczba == 1) console.log("nie")
+if (liczba == 2) console.log("tak")
+for (var i=2; i<liczba; i++) {
 
-
-for (var i=0; i<liczba; i++) {
-  if(liczba%dzielnik==0) {
+  if (liczba % i == 0) {
     console.log("nie")
     break;
   }
-  if (liczba%dzielnik!=0) {
+  if (liczba % i != 0 ) {
     console.log("tak")
     break;
   }
-  dzielnik++;
 }
---
+++ Zad_cw_3/Zadanie1.js
@@ -0,0 +1,24 @@
+var array = [2,6,-2];
+var max = 0;
+var min = 0;
+var sum = 0;
+var less = 0;
+var more = 0;
+
+for (var i=0; i < array.length; i++) {
+    if (max <= array[i]) max = array[i];
+    if (min >= array[i]) min = array[i];
+    sum += array[i];
+    var average = sum / array.length;
+    console.log(average)
+    if (average < array[i]) more++;
+    if (average == array[i]) more--;
+    if (average > array[i]) less++;
+}
+console.log("Tablica:", array.join(" | "))
+console.log("Najwieksza liczba z tablicy to:", max);
+console.log("Najmniejsza liczba z tablicy to:", min);
+console.log("Średnia liczb to:", average);
+console.log("Liczba mniejszych od średniej liczb to:", less);
+console.log("Liczba większych od średniej liczb to:", more);
+console.log("Tablica w odwrotnej kolejności:", array.reverse().join(" | "));
