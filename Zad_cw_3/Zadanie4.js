6cb63eeed4301b96db42f81fe63f82f065d36425
Renata <r.wesolowska@devesoft.pl>
2019-05-14 08:18:22 +0200

    Kontynuacja

--- Zad_cw_3/Zadanie4.js
+++ Zad_cw_3/Zadanie4.js
@@ -10,3 +10,10 @@ while ((decNum < -2147483648) || (decNum > 2147483647)) {
   console.log("Podana liczba nie należy do tego przedziału (-2147483648 - 2147483647)");
   decNum = readlineSync.questionInt("Podaj liczbę całkowitą: ");
 }
+
+// deklaracja tablicy pomocniczej i znaku liczby
+var binArray = [];
+var char = decNum < 0 ? 1 : 0;
+
+result += "Liczba" + decNum + "binarnie: ";
+decNum = decNum * (decNum < 0 ? -1 : 1);
