054e9c7086f4a6cd123deb8d2bedfc51e321591d
Renata <r.wesolowska@devesoft.pl>
2019-05-14 08:39:20 +0200

    wykonane

--- Zad_cw_3/Zadanie4.js
+++ Zad_cw_3/Zadanie4.js
@@ -8,12 +8,28 @@ var result = "";
 var decNum = readlineSync.questionInt("Podaj liczbę całkowitą: ");
 while ((decNum < -2147483648) || (decNum > 2147483647)) {
   console.log("Podana liczba nie należy do tego przedziału (-2147483648 - 2147483647)");
-  decNum = readlineSync.questionInt("Podaj liczbę całkowitą: ");
+var decNum = readlineSync.questionInt("Podaj liczbę całkowitą: ");
 }
 
 // deklaracja tablicy pomocniczej i znaku liczby
 var binArray = [];
 var char = decNum < 0 ? 1 : 0;
 
-result += "Liczba" + decNum + "binarnie: ";
+result += "Liczba " + decNum + " binarnie: ";
 decNum = decNum * (decNum < 0 ? -1 : 1);
+
+while(true){
+  binArray.unshift(decNum % 2);
+  decNum = Math.floor(decNum / 2);
+
+  if(decNum < 1){
+    binArray.unshift(char);
+    break;
+  }
+}
+
+for(var i = 0; i < binArray.length; i++){
+  result += binArray[i] + (i === 0 ? "." : "")
+}
+
+console.log(result);
