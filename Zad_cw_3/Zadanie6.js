1448f29b3dffdb8d71b4afc85f377d2d9ff36418
Renata <r.wesolowska@devesoft.pl>
2019-05-14 14:03:05 +0200

    wykonane (brak ładnego wyniku)

--- Zad_cw_3/Zadanie6.js
+++ Zad_cw_3/Zadanie6.js
@@ -1,30 +1,57 @@
 const readlineSync = require('readline-sync');
+// </editor-fold>
 
-//Zmienna końcowa
+//Zmienna finalnie wyświetlana jako wynik działania programu
 var result = "";
 
-//wartości podawane przez użytkownika
-
+// <editor-fold desc="Input">
+//Inputy podawane przez użytkownika
 var n = readlineSync.questionInt("Podaj liczbę (> 0): ");
+// </editor-fold>
 
-var array = new Array((n + 1));
-function char (x,y){
-  //funkcja sprawdzająca czy liczby są względnie pierwsze
+// <editor-fold desc="Operacje wykonywane przez program" >
+// Algorytm Euklidesa
+var euklid = function (n1, n2) {
+  // Ustalenie, która liczba jest większa
+  let a;
+  let b;
+  if (n1 >= n2) {
+    a = n1;
+    b = n2;
+  } else {
+    b = n1;
+    a = n2;
+  }
+
+  // Obliczenia
+  while (b !== 0) {
+    let c = a % b;
+    a = b;
+    b = c;
+  }
+
+  return a;
 }
-
-//tworzenie górnego i lewego paska tablicy
+var array = new Array((n + 1));
 for (var y = 0; y <= n; y++) {
   array[y] = [];
   for (var x = 0; x <= n; x++) {
     array[y][0] = y;
     array[0][x] = x;
+    array[y][x] = (euklid(array[0][x],array[y][0]) === 1);
+    array[y][x] = array[y][x] ? "+" : ".";
   }
 }
+console.log(array)
+/*for (var y = 0; y <= n; y++) {
+  array[y] = [];
+  for (var x = 0; x <= n; x++) {
+    result += array[y][x] + " | "";
+  }
+result += "\n";
+}
+*/
+// Konstrukcja wiadomości końcowej
 
-//wypełnianie tablicy + i -
-for (var y = 1; y <= n; y++) {
-  for (var x = 1; x <= n; x++) {
-    char(x,y);
-  }
-}
-console.log(array.join("\n"))
+//Wyświetlanie wyniku działania
+console.log(result);
